d236ad70ba14f8307b07628e0b1fd7a0de039d71
Dylan <Quantum64Development@gmail.com>
2018-03-15 00:29:50 -0400

    Add comment

--- src/main/java/co/q64/jstx/opcode/Opcodes.java
+++ src/main/java/co/q64/jstx/opcode/Opcodes.java
@@ -6,7 +6,9 @@ import javax.inject.Singleton;
 
 @Singleton
 public class Opcodes implements Consumer<Opcode> {
-	
+	// 0 - 55 standard opcodes
+	// 56 - 154 two character
+	// 155 - 255 three character
 	
 	@Override
 	public void accept(Opcode t) {
